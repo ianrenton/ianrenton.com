44445bab7cb71ab3c4349ce4a5b0d815e5e41ea6
Ian Renton <ian@ianrenton.com>
2020-12-27 14:20:48 +0000

    Minor recaptcha tweaks

--- js/comments.js
+++ js/comments.js
@@ -11,54 +11,45 @@ $('#comment-form-show-preview').change(function() {
 // Show Captcha and update preview on key up, so when the user types something in the comment field
 function commentTextFieldOnKeyUp() {
   $('.g-recaptcha').show();
-  $('#commentpreviewtoggle').show();
   $('#commentpreview').html(converter.makeHtml($('#comment-form-message').val()));
 }
 
 // Static comments submission code
 // from: https://github.com/eduardoboucas/popcorn/blob/gh-pages/js/main.js
-(function ($) {
-  var $comments = $('.js-comments');
+$('.js-form').submit(function () {
+  var form = this;
 
-  $('.js-form').submit(function () {
-    var form = this;
+  $("#comment-form-submit").html('Please Wait...');
+  $(form).addClass('disabled');
 
-    $("#comment-form-submit").html('Please Wait...');
-    $(form).addClass('disabled');
-
-    $.ajax({
-      type: $(this).attr('method'),
-      url:  $(this).attr('action'),
-      data: $(this).serialize(),
-      contentType: 'application/x-www-form-urlencoded',
-      success: function (data) {
-        showModal('Comment submitted', 'Thanks! Your comment is pending. It will appear when approved.');
-        $('.js-modal-text').removeClass('js-modal-text-error');
-        $("#comment-form-submit").html("Submit");
-        $(form)[0].reset();
-        $(form).removeClass('disabled');
-        grecaptcha.reset();
-      },
-      error: function (err) {
-        console.log(err);
-        var ecode = (err.responseJSON || {}).errorCode || "unknown";
-        showModal('Error', 'An error occured.<br>[' + ecode + ']');
-        $('.js-modal-text').addClass('js-modal-text-error');
-        $("#comment-form-submit").html("Submit");
-        $(form).removeClass('disabled');
-        grecaptcha.reset();
-      }
-    });
-    return false;
+  $.ajax({
+    type: $(this).attr('method'),
+    url:  $(this).attr('action'),
+    data: $(this).serialize(),
+    contentType: 'application/x-www-form-urlencoded',
+    success: function (data) {
+      showModal('Comment submitted', 'Thanks! Your comment is pending. It will appear when approved.');
+      $('.js-modal-text').removeClass('js-modal-text-error');
+      $("#comment-form-submit").html("Submit");
+      $(form)[0].reset();
+      $(form).removeClass('disabled');
+      grecaptcha.reset();
+    },
+    error: function (err) {
+      console.log(err);
+      var ecode = (err.responseJSON || {}).errorCode || "unknown";
+      showModal('Error', 'An error occured.<br>[' + ecode + ']');
+      $('.js-modal-text').addClass('js-modal-text-error');
+      $("#comment-form-submit").html("Submit");
+      $(form).removeClass('disabled');
+      grecaptcha.reset();
+    }
   });
+  return false;
+});
 
-  $('.js-close-modal').click(function () {
-    $('body').removeClass('show-modal');
-  });
-
-  function showModal(title, message) {
-    $('.js-modal-title').text(title);
-    $('.js-modal-text').html(message);
-    $('body').addClass('show-modal');
-  }
-})(jQuery);
+function showModal(title, message) {
+  $('.js-modal-title').text(title);
+  $('.js-modal-text').html(message);
+  $('body').addClass('show-modal');
+}
