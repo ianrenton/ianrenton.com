7e2c53e81d4ce1c2d160c1df848eb94e331d401a
Ian Renton <ian@ianrenton.com>
2021-01-23 11:54:48 +0000

    First try at Windows 3.1 style

---
+++ js/code.js
@@ -0,0 +1,33 @@
+$( function() {
+    // Window drag
+    $( ".window" ).draggable({ handle: "div.windowtitle" });
+
+    // Window resize
+    $( ".window" ).resizable({ handles: "all", alsoresize: ".windowinner" });
+
+    // Window close
+    $('.windowclose').on("click", function () { $(this).parents('div.window').hide(); });
+
+    // Window click-to-bring-to-top
+    (function() {
+        var highest = 100;
+
+        $.fn.selectWindow = function() {
+            // Make top
+            this.css('z-index', ++highest);
+            // Make this window selected and others not
+            this.addClass('selectedwindow');
+            $('.window').not(this).each(function(){
+                $(this).removeClass('selectedwindow');
+             });
+        };
+    })();
+    $('.window').click(function() {
+        $(this).selectWindow();
+    });
+
+    // Icon single click
+    $('.icon').click(function() {
+      $(this).find('p').toggleClass('highlight');
+    });
+} );
