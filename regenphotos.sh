366f15d6d3151935ba8aa921a68dc77150d91317
Ian Renton <ian@ianrenton.com>
2022-07-03 12:04:05 +0100

    Use pregenerated thumbnails in yearly photo galleries rather than the whole image

---
+++ regenphotos.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script to regenerate thumbnails after adding new photos.
+# Requires ImageMagick.
+
+REL_PATH="photography/images"
+YEAR_REGEX="[0-9]{4}"
+
+cd $REL_PATH
+for d in */; do
+  if [[ $d =~ $YEAR_REGEX ]];
+  then
+      echo "Processing $d..."
+      cd $d
+      mkdir -p thumbs
+      for file in *; do
+        if [ ! -d "$file" ]; then
+          convert $file -resize 320x280^ -gravity center -extent 320x280 thumbs/$file
+        fi
+      done
+      cd ..
+  fi
+done
+cd ..
